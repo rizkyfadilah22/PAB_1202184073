df0fe1504e0a6ff6354ec05c56487ca528a26a90
rizkyfadilah22 <rizkyfadilah22@gmail.com>
2021-12-15 16:53:34 +0700

    commit tugas login register

---
+++ LoginRegisterNotif/app/src/main/java/com/example/loginregisternotif/Register.java
@@ -0,0 +1,44 @@
+package com.example.loginregisternotif;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+        final EditText userName = (EditText) findViewById(R.id.etNewName);
+        final EditText email = (EditText) findViewById(R.id.etNewEmail);
+        final EditText password = (EditText) findViewById(R.id.etNewPassword);
+        Button btnRegister = (Button) findViewById(R.id.btnNewRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences preferences = getSharedPreferences("MYPREFS", MODE_PRIVATE);
+                String newUser = userName.getText().toString();
+                String newPassword = password.getText().toString();
+                String newEmail = email.getText().toString();
+
+                SharedPreferences.Editor editor = preferences.edit();
+
+                editor.putString(newUser + newPassword + "data", newUser + "\n" + newEmail);
+                editor.commit();
+
+                Intent loginScreen = new Intent(Register.this, MainActivity.class);
+                startActivity(loginScreen);
+
+
+            }
+        });
+    }
+}
